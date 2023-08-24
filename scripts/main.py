364e17c11afa9ca25138c630996bf0934500a931
fedegogo <fegome@Pulse.localdomain>
2023-08-24 00:34:36 -0500

    Add strucuture folders and import libraries

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+# https://ihsavru.medium.com/how-to-build-your-own-custom-chatgpt-using-python-openai-78e470d1540e
+# https://platform.openai.com/docs/libraries/python-library 
+
+import openai
+import os
+
+openai.ChatCompletion()
+print ('Hello AI');
