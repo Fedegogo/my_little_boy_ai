0a2020b50dfe6e5da154bc72b57f49137a43f31e
fedegogo <fegome@Pulse.localdomain>
2023-08-25 01:21:03 -0500

    Create my own chat bot

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,25 @@
-# https://ihsavru.medium.com/how-to-build-your-own-custom-chatgpt-using-python-openai-78e470d1540e
-# https://platform.openai.com/docs/libraries/python-library 
-
 import openai
 import os
 
-openai.ChatCompletion()
-print ('Hello AI');
+# Load the API key from an environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+messages = []
+
+# Prompt user for input
+message = input("User: ")
+print(message)
+
+# Add each new message to the list
+messages.append({"role": "user", "content": message})
+print(messages)
+
+# Request gpt-3.5-turbo for chat completion
+response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+# Print the response and add it to the messages list
+chat_message = response['choices'][0]['message']['content']
+print(f"Bot: {chat_message}")
